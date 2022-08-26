f7269c1a84d5fb6301894850d58bc4294fbdd010
sidaroz <sidarozbek96@gmail.com>
2022-08-26 02:12:02 +0100

    add Header component with two buttons, help and darkmode

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,37 @@
 import React from "react";
-import { Heading, Flex } from "@chakra-ui/react";
+import {
+  Heading,
+  Flex,
+  Box,
+  Spacer,
+  ButtonGroup,
+  Button,
+} from "@chakra-ui/react";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { CgDarkMode } from "react-icons/cg";
 
 function Header() {
   return (
-    <Flex>
-      <Heading as={"h1"} size={"xl"}>
-        Hexle
-      </Heading>
-    </Flex>
+    <>
+      <Flex
+        width={"100%"}
+        justifyContent={"end"}
+        borderBottom={"1px solid grey"}
+        padding={"6px 0"}
+      >
+        <Heading as={"h1"} size={"xl"} margin={"0 auto"} paddingLeft={"4%"}>
+          Hexle
+        </Heading>
+        <Flex>
+          <Box as="button">
+            <AiOutlineQuestionCircle size={"2em"} />
+          </Box>
+          <Box as="button">
+            <CgDarkMode size={"2em"} />
+          </Box>
+        </Flex>
+      </Flex>
+    </>
   );
 }
 
